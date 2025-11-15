330093dd999e3d485afb65e494100fca6acabd5d
sebastianUCC2024 <juans.patino@campusucc.edu.co>
2025-11-15 11:20:30 -0500

    Agregar módulo common con DTOs y configuración compartida

---
+++ common/src/main/java/com/agrigo/common/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package com.agrigo.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String email;
+    private String name;
+    private String role;
+    private String phone;
+}
